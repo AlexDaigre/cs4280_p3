6c6d5368fd99f4b04710b5b57f32cef84bbdd535
Alex Daigre <alexdaigre@gmail.com>
2018-11-26 06:46:09 -0600

    added more info to output

--- testTree.cpp
+++ testTree.cpp
@@ -19,7 +19,14 @@ using namespace std;
             return; 
         }
 
-        cout << getSpacesFromInt(level) << node->nodeType << "\n";
+        cout << getSpacesFromInt(level) << node->nodeType
+            << ": " << getTokenName(node->token0.tokenId) 
+            << "/" << node->token0.tokenInstance
+            << ": " << getTokenName(node->token1.tokenId) 
+            << "/" << node->token1.tokenInstance
+            << ": " << getTokenName(node->token2.tokenId) 
+            << "/" << node->token2.tokenInstance
+            << "\n";
         preOrderRecursiveBody(node->child0, ++level); 
         preOrderRecursiveBody(node->child1, ++level);
         preOrderRecursiveBody(node->child2, ++level); 
@@ -35,7 +42,14 @@ using namespace std;
         postOrderRecursiveBody(node->child1, ++level);
         postOrderRecursiveBody(node->child2, ++level); 
         postOrderRecursiveBody(node->child3, ++level);
-        cout << getSpacesFromInt(level) << node->nodeType << "\n";
+        cout << getSpacesFromInt(level) << node->nodeType
+            << ": " << getTokenName(node->token0.tokenId) 
+            << "/" << node->token0.tokenInstance
+            << ": " << getTokenName(node->token1.tokenId) 
+            << "/" << node->token1.tokenInstance
+            << ": " << getTokenName(node->token2.tokenId) 
+            << "/" << node->token2.tokenInstance
+            << "\n";
     }
 
     void traversePostorder(Node* node) {
